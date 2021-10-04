c4f4b9f5dee5f8173ccd1d2321c2d3fe0219fa39
dcerjan <domagoj.cerjan@gmail.com>
2021-10-04 22:44:41 +0200

    Fix ArenaManager

--- src/root/ArenaManager.gd
+++ src/root/ArenaManager.gd
@@ -1,4 +1,8 @@
 extends Node
 
-func add_arena(arena: Spatial) -> void:
+func set_arena(arena: Spatial) -> void:
+  var root = get_node('/root/World/Arena')
+  for c in root.get_children():
+    root.remove_child(c)
+    c.queue_free()
   get_node('/root/World/Arena').add_child(arena)
--- src/root/Root.gd
+++ src/root/Root.gd
@@ -3,4 +3,4 @@ extends Spatial
 const Arena := preload('res://src/arena/Arena.tscn')
 
 func _ready() -> void:
-  ArenaManager.add_arena(Arena.instance())
+  ArenaManager.set_arena(Arena.instance())
