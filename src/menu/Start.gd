02bc28d96e389ac73ad968af82c288ca844ae322
dcerjan <domagoj.cerjan@gmail.com>
2021-10-04 23:58:18 +0200

    Mouse html stuff

--- src/menu/Start.gd
+++ src/menu/Start.gd
@@ -2,5 +2,6 @@ extends Button
 
 
 func _on_Start_pressed():
+  Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
   var arena = load('res://src/arena/Arena.tscn').instance()
   ArenaManager.set_arena(arena)
